773405395e6cb2e1abf20e3e26c84de649866566
moulin-louis <louis.moulin@outlook.fr>
2024-10-26 14:42:33 +0200

    add eslint

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,10 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2024-04-03',
-  devtools: { enabled: true }
-})
+  compatibilityDate: "2024-04-03",
+  modules: ["@nuxt/eslint", "@nuxtjs/tailwindcss"],
+  typescript: {
+    typeCheck: true,
+    strict: false,
+  },
+  devtools: { enabled: true },
+});
