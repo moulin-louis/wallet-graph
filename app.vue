edabb5fe6442be56e5ba2eafbf31d02b6c8e1c21
moulin-louis <louis.moulin@outlook.fr>
2024-10-26 14:56:26 +0200

    add primevue

--- app.vue
+++ app.vue
@@ -2,5 +2,7 @@
   <div>
     <NuxtRouteAnnouncer />
     <NuxtWelcome />
+    <div class="bg-red-900">Hello</div>
+    <Button label="Check" icon="pi pi-check" />
   </div>
 </template>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,10 +1,15 @@
+import Aura from "@primevue/themes/aura";
+
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: "2024-04-03",
-  modules: ["@nuxt/eslint", "@nuxtjs/tailwindcss"],
-  typescript: {
-    typeCheck: true,
-    strict: false,
+  modules: ["@nuxt/eslint", "@nuxtjs/tailwindcss", "@primevue/nuxt-module"],
+  primevue: {
+    options: {
+      theme: {
+        preset: Aura,
+      },
+    },
   },
   devtools: { enabled: true },
 });
